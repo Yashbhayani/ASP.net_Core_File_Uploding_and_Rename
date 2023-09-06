8d8d47c06d6b3c3ad6642a8e07670fddb3ee2223
Yash Bhayani <yashbhayani3007@gmail.com>
2023-09-06 15:53:30 +0530

    first commit

---
+++ Fileuloding/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+
+namespace Fileuloding.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : Controller
+    {
+        [Route("/home")]
+        [HttpPost]
+        public IActionResult Index(IFormFile file)
+        {
+            try
+            {
+                var folderName = Path.Combine("Images", "Userprofile");
+                var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+                if (file.Length > 0)
+                {
+                    var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                    string renameFile = Convert.ToString(Guid.NewGuid()) + "." + fileName.Split('.').Last();
+                    var fullPath = Path.Combine(pathToSave, renameFile);
+                    var dbPath = Path.Combine(folderName, fileName);
+
+                    using (var stream = new FileStream(fullPath, FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                    }
+                    return Ok(new { renameFile });
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }catch(Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+    }
+}
